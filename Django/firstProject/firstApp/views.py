3f3501ef83a31b71fbfb7d725359ca7519a6b987
ITechSeeker <itechseeker.com@gmail.com>
2021-08-27 21:05:45 +0700

    Revert "Implement a basic app to test django instalation"
    This reverts commit 2cb79d5210437b7948fde52557b9ce5a0646a6ce.

--- Django/firstProject/firstApp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def getInfo(request):
-    data=[{'name':'Tom', 'age':30},
-          {'name':'David', 'age':45},
-          {'name':'Lisa', 'age':28}]
-    return HttpResponse(data)
